feab97b2f0d6be8c7cba2d6a84d5130a45917458
AngelCaido-ai <a-filatoff@mail.ru>
2020-10-05 21:00:06 +0300

    Homework 05.10.20

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static int Fibo(int n)
+        {
+            if (n > 1)
+                return Fibo(n - 1) + Fibo(n - 2);
+            else
+                return 1;
+        }
+
+
+        static void Main()
+        {
+            Console.WriteLine("Введите число: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+                Console.Write(Fibo(i) + " ");
+            Console.WriteLine();
+        }
+    }
+}
