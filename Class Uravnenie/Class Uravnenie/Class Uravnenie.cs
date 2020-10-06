7378b87c82ef3dda6789ffbbbec02383b5e7be3c
Nikitka <user123@gmail.com>
2020-10-06 19:06:49 +0300

    fdf

---
+++ Class Uravnenie/Class Uravnenie/Class Uravnenie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace class_uravnenie
+{
+    class Uravnenie
+    {
+        public double a;
+        public double b;
+
+        public double Reshenie(double a, double b)
+        {
+            return b / a;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uravnenie mU = new Uravnenie();
+            Console.WriteLine("Введем данные уравния вида a*x=b");
+            Console.WriteLine("a = ");
+            mU.a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("b = ");
+            mU.b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Получили уравнение: " + mU.a+"*x="+mU.b);
+            if (mU.a == 0)
+            {
+                Console.WriteLine("Уравнение не имеет корней");
+                return;
+            }
+            Console.WriteLine("X = " + mU.Reshenie(mU.a, mU.b));
+        }
+    }
+}
---
+++ Fibonachchi/FIbonachchi/Fibonachchi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace FIbonachchi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int [n];
+            a[0] = 1;
+            a[1] = 1;
+            Console.WriteLine(a[0]);
+            Console.WriteLine(a[1]);
+            for (int i = 2; i < n; i++)
+            {
+                a[i] = a[i - 1] + a[i - 2];
+                if (a[i] > n) { break; }
+                Console.WriteLine(a[i]);
+            }
+        }
+    }
+}
