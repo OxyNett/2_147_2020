0c75b7ea5ec267bc8992b5337a1ead67bc0466a2
unknown <milshegol@gmail.com>
2020-10-06 19:04:03 +0300

    HouseWork

---
+++ 1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число");
+            int number = int.Parse(Console.ReadLine());
+            double octatok = number % 2;
+            if (octatok == 0)
+                Console.WriteLine("число четное");
+            else
+                Console.WriteLine("Число нечетное");
+
+        }
+    }
+}
---
+++ 2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vvdei chislo");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int odin = 1;
+            Console.Write("{0} ", odin);
+            int dva = 1;
+            Console.Write("{0} ", dva);
+            int sum = 0;
+            while (number >= sum)
+            {
+                sum = odin + dva;
+                if (number >= sum)
+                    Console.Write("{0} ", sum);
+
+                odin = dva;
+                dva = sum;
+            }
+        }
+    }
+}
