0a86555f1c1e6ac8b02c0432f0a37ab8853bd99e
lyub1m <danil.lyubimov.2002@gmail.com>
2020-10-06 20:19:55 +0300

    first

---
+++ Program2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Задание_2_2вар_
+{
+    class Program
+    {
+        static int Fibo(int n)
+        {
+            if (n > 1)
+                return Fibo(n - 1) + Fibo(n - 2);
+            else
+                return 1;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nВведите количество элементов: ");
+            int p = int.Parse(Console.ReadLine());
+            int n = p;
+            for (int i = 0; i < n; i++)
+                Console.Write(Fibo(i) + " "); 
+            Console.WriteLine("\nВведите номер числа: ");
+            int Number = int.Parse(Console.ReadLine());
+            Number--;
+            for (int i = 0; i <= Number; i++)
+            {
+                Console.WriteLine(Fibo(i));
+            }
+            Console.ReadKey();
+        }
+    }
+}
