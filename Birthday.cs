2e7f749cd77726a3a2f817f6569d19bb51585e48
Vlad_Prorokov <olo098olo@mail.ru>
2020-11-03 20:31:26 +0300

    birthday

---
+++ Birthday.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Denrojdeniya
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите дату в формате xx.xx.xxxx");
+            string birthDate = Console.ReadLine();
+            DateTime BirthDate = DateTime.Parse(birthDate);
+            int age = DateTime.Now.Year - BirthDate.Year;
+            if (age >= 0)
+            {
+                if (DateTime.Now.Month < BirthDate.Month)
+                {
+                    age--;
+                    if (age >= 0)
+                    {
+                        Console.WriteLine(age);
+                    }
+                    else { Console.WriteLine("Эта дата еще не наступила."); }
+                }
+                else if (DateTime.Now.Month == BirthDate.Month && DateTime.Now.Day < BirthDate.Day)
+                {
+                    if (age >= 0)
+                    {
+                        Console.WriteLine(age);
+                    }
+                    else { Console.WriteLine("Эта дата еще не наступила."); }
+                }
+                else if (DateTime.Now.Year == BirthDate.Year && DateTime.Now.Month == BirthDate.Month && DateTime.Now.Day == BirthDate.Day)
+                {
+                    Console.WriteLine("eto je segodnya");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Эта дата еще не наступила.");
+            }
+        }
+    }
+}
