930782b9915179830d47018ddbbb5f484f61a7e8
lyub1m <danil.lyubimov.2002@gmail.com>
2020-11-03 20:10:26 +0300

    task2

---
+++ д.з №1/Задание 1/Задание 1/Program1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0)
+                Console.WriteLine("Число является чётным");
+            else
+                Console.WriteLine("Число не является чётным");
+            Console.ReadKey();
+        }
+    }
+}
