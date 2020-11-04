534faaa5955402480441a3f963710a35f51c0b84
Sony <61905495+sonymanetov@users.noreply.github.com>
2020-11-04 10:28:15 +0400

    Child

---
+++ Child.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace Task
+{
+    class Child
+    {
+        private bool hungry = false;
+        private bool play = false;
+        private bool sleep = false;
+
+        public Child(bool hungry, bool play, bool sleep)
+        {
+            this.hungry = hungry;
+            this.play = play;
+            this.sleep = sleep;
+
+        }
+
+
+        public void child_hungry()
+        {
+            if (!hungry)
+            {
+                Console.WriteLine("The child isnt hungry");
+            }
+            else
+            {
+                Console.WriteLine("You've feed the child! Good job!");
+                hungry = false;
+            }
+        }
+
+        public void child_sleep()
+        {
+            if (sleep == true)
+            {
+                Console.WriteLine("The child dont wanna sleep");
+            }
+            else
+            {
+                Console.WriteLine("You put the child to bed.");
+                sleep = true;
+            }
+        }
+
+
+        public void child_play()
+        {
+            if (play == true)
+            {
+                Console.WriteLine("The child tired to play. He want to rest.");
+            }
+            else
+            {
+                Console.WriteLine("You are playing with the child.");
+                play = true;
+            }
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press W to feed the child");
+            Console.WriteLine("Press A to put him to bed");
+            Console.WriteLine("Press D to play with child");
+            Console.WriteLine("Press S to close this app");
+
+            Child child1 = new Child(true, false, true);
+
+            string a = Console.ReadLine();
+
+            while (a != "s")
+            {
+                if (a == "w")
+                {
+                    child1.child_hungry();
+                }
+
+                else if (a == "a")
+                {
+                    child1.child_sleep();
+                }
+
+                else if (a == "d")
+                {
+                    child1.child_play();
+                }
+                else
+                {
+                    Console.WriteLine("Ты промазал по клаве, давай еще раз");
+                }
+                a = Console.ReadLine();
+            }
+            Console.ReadKey();
+        }
+
+    }
+    
+}
