00242c6ecdc15da42953b60c98f1245083b6343e
lyub1m <danil.lyubimov.2002@gmail.com>
2020-12-01 15:00:41 +0300

    BankAccount

---
+++ BankAccountNS/BnakTests/BankAccountTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BnakTests
+{
+    class BankTests
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
